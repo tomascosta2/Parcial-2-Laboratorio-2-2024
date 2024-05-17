77056e16e98e49cafa4c1c7e6bcffcd9204f62c4
tomas <tomascostapp@gmail.com>
2024-05-17 16:24:06 -0300

    Inicial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Ingrese la accion a realizar: \n" +
+                "1-Vender/Cobrar un producto\n" +
+                "2-Ingreso de mercadería\n" +
+                "3-Pago a proveedor\n" +
+                "4-Consulta de ventas\n" +
+                "        a- Diaria\n" +
+                "        b- Mensual\n" +
+                "5-Balance (mostrar ganancias y pérdidas)\n" +
+                "6-Solicitar una comanda a la cocina.\n" +
+                "7-Pagar cuenta\n" +
+                "8-Informacion estadística de platos más pedidos."
+        );
+
+        int accion = Integer.parseInt(sc.next());
+
+    }
+}
